114b03fafcdae61f58ba094d98981c1738532eb9
Scott Sargent <ssargent@paylocity.com>
2017-05-04 14:09:46 -0400

    adding lexer

--- monkey/token/token.go
+++ monkey/token/token.go
@@ -1,8 +1,36 @@
 package token
 
+// TokenType dictates what type of token we're dealing with.  WTSE-001
 type TokenType string
 
+// Token is a struct that holds a token
 type Token struct {
 	Type    TokenType
 	Literal string
 }
+
+// SYMBOLS
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// identifiers + literals
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+
+	// keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
